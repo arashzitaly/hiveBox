af856486d48cc67a057c0c39fb18ca7e628c5de9
Arash <arashziaee16@gmail.com>
2026-03-21 12:02:44 +0100

    feat(api): add /version endpoint with Flask

--- src/app.py
+++ src/app.py
@@ -1,4 +1,14 @@
+from flask import Flask, jsonify
+
 __version__ = "0.0.1"
 
+app = Flask(__name__)
+
+
+@app.get("/version")
+def version():
+    return jsonify({"version": __version__})
+
+
 if __name__ == "__main__":
-    print(f"v{__version__}")
+    app.run(host="0.0.0.0", port=8080)
